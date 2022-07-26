02300cf03a42ed832c9dc16e77255073b6c731ef
Nimish Dudhe <77407701+nimishdudhe01@users.noreply.github.com>
2022-07-26 08:46:28 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,8 +3,13 @@
 echo [+] Assembling
 nasm -f elf32 -o $1.o $1.asm
 
-echo [+] Linking
-ld -o $1 $1.o -m elf_i386
+if [ $? == 0 ]
+then
+  echo [+] Linking
+  ld -o $1 $1.o -m elf_i386
+else
+  exit
+fi
 
 if [ $? == 0 ]
   then echo [+] Done
