e0c0cf49d734c256315037b86fdd79657a44b954
Nimish Dudhe <77407701+nimishdudhe01@users.noreply.github.com>
2022-07-26 09:58:45 +0530

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo [+] Assembling
+nasm -f elf32 -o $1.o $1.asm
+
+if [ $? == 0 ]
+then
+  echo [+] Linking
+  ld -o $1 $1.o -m elf_i386
+else
+  echo "An Error Occured"
+  exit
+fi
+
+if [ $? == 0 ]
+  then echo [+] Done
+else
+  echo "An Error Occured"
+fi
